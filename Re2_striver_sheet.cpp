0ff00c6e99df77e1698e150a04a2caf93fb9ec9e
Aman Kumar Thakur <ssamanthakur@gmail.com>
2023-12-26 06:49:39 +0530

    basic problems on reccursion disscussed in re 2

---
+++ Re2_striver_sheet.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+// print name 5 times
+// void printntimesname(int n,string name)
+// {
+//     if(n==1)
+//     {
+//         return;
+//     }
+//     cout<<name<<endl;
+//     printntimesname(n-1,name);
+// }
+
+// print linearly from 1 to N using backtracking
+// void printlinearly12n(int n)
+// {
+//     if(n==0)
+//     {
+//         return;
+//     }
+//     printlinearly12n(n-1);
+//     cout<<n<<" ";
+// }
+
+// print linearly N to 1 using backtracking
+// void printlinearlyn21(int n)
+// {
+//     if(n==0)
+//     {
+//         return;
+//     }
+//     cout<<n<<" ";
+//     printlinearlyn21(n-1);
+// }
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    // string name;
+    // cin>>name;
+    // printntimesname(n,name);
+    // printlinearly12n(n);
+    // printlinearlyn21(n);
+
+    return 0;
+}
