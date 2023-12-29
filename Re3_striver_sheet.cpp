1f4752df7737a24afbeef5dbed28c13f65e7b8a5
Aman Kumar Thakur <ssamanthakur@gmail.com>
2023-12-29 16:45:52 +0530

    Recursion lecture 3 solved questions

---
+++ Re3_striver_sheet.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+// Sum of n numbers
+// int sum(int n)
+// {
+//     if(n==0)
+//     {
+//         return 0;
+//     }
+//     return n+sum(n-1);
+// }
+
+// N factorial
+// int factoriaal(int n)
+// {
+//     if(n==0)
+//     {
+//         return 1;
+//     }
+//     return n*factoriaal(n-1);
+// }
+
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    // cout<<sum(n);
+    // cout<<factoriaal(n);
+    return 0;
+}
