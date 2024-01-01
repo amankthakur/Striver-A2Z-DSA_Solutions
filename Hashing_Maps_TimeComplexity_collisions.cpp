689861f335aa4713d4e85b9769f25deec7808244
Aman Kumar Thakur <ssamanthakur@gmail.com>
2024-01-02 04:39:00 +0530

    did number and character hassing

---
+++ Hashing_Maps_TimeComplexity_collisions.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // implementing number hashing or index hashing
+    // int n;
+    // cout<<"Enter array length"<<endl;
+    // cin>>n;
+    // int arr[n];
+    // cout<<"Enter array elements"<<endl;
+    // for(int i=0;i<n;i++)
+    // {
+    //     cin>>arr[i];
+    // } 
+    // // precompute
+    // int hash[13]={0};
+    // for(int i=0;i<n;i++)
+    // {
+    //     hash[arr[i]]+=1;
+    // }
+    // // fetchin
+    // int q;
+    // cout<<"Enter query element Whose no. of terms we are finding"<<endl;
+    // cin>>q;
+    // cout<<hash[q];
+    // return 0;
+
+    // // Character Hashing
+    //  string s;
+    //  cout<<"Enter your string \n";
+    //  cin>>s;
+    //  int n=s.size();
+    // //  precompute
+    // int hash[256]={0};
+    // for(int i=0;i<n;i++)
+    // {
+    //     hash[((int)(s[i]))]+=1;
+    // }
+    // // fetching
+    // char q;
+    // cout<<"Enter query element Whose no. of terms we are finding"<<endl;
+    // cin>>q; 
+    // cout<<hash[(int)(q)];
+    // return 0;
+}
