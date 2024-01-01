78972992a283cc5b4c7f68c181650b32314e04a8
Aman Kumar Thakur <ssamanthakur@gmail.com>
2024-01-02 02:47:59 +0530

    did Re5 lecture questions and removed unrequired files

--- Re5_striver_sheet.cpp
+++ Re5_striver_sheet.cpp
@@ -1,12 +1,23 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-// funtion to print fibonachi no.
-int fibo()
-{
-
-}
+// funtion to print Nth fibonachi no.
+// int fibo(int n)
+// {
+//     if(n==0)
+//     {
+//         return 0;
+//     }
+//     if(n==1)
+//     {
+//         return 1;
+//     }
+//     return fibo(n-1)+fibo(n-2);
+// }
 int main()
 {
-
+    int n;
+    cout<<"Enter the Nth term we need to find of fibonachi"<<endl;
+    cin>>n;
+    // cout<<fibo(n)<<" is the "<<n<<"th fibonachi no."<<endl;
 }
