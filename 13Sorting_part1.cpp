c8eeea5f51a05e714bf92ac830bb4f8f34516e4f
Aman Kumar Thakur <ssamanthakur@gmail.com>
2024-01-11 03:18:38 +0530

    done

--- 13Sorting_part1.cpp
+++ 13Sorting_part1.cpp
@@ -1,6 +1,72 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    return 0;
+    // int n;
+    // cout << "no. of elements in array\n";
+    // cin >> n;
+    // int arr[n];
+    // cout << "enter elements in array\n";
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
+
+    // selection sort
+    // for(int i=0;i<n-1;i++)
+    // {
+    //     for(int k=(i+1);k<n;k++)
+    //     {
+    //         if(arr[i]>arr[k])
+    //         {
+    //             swap(arr[i],arr[k]);
+    //         }
+    //     }
+    // }
+    // cout<<"sorted array is:\n";
+    // for(int i=0;i<n;i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+
+    //  Bubble Sort
+    // for (int i = n - 1; i >= 1; i--)
+    // {
+    //     int didSwap = 0;
+    //     for (int j = 0; j <= i-1; j++)
+    //     {
+    //         if (arr[j] > arr[j + 1])
+    //         {
+    //             swap(arr[j], arr[j + 1]);
+    //             didSwap=1;
+    //         }
+    //     }
+    //     if(didSwap==0)
+    //     {
+    //         break;
+    //     }
+    //     cout<<"runs\n";
+    // }
+    // cout<<"sorted array is:\n";
+    // for(int i=0;i<n;i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+
+    // Insertion Sort
+    // for(int i=0;i<n-1;i++)
+    // {
+    //     int j=i;
+    //     while(j>0&&arr[j-1]>arr[j])
+    //     {
+    //         swap(arr[j-1],arr[j]);
+    //         j--;
+    //     }
+    // }
+    // cout<<"sorted array is:\n";
+    // for(int i=0;i<n;i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+    // return 0;
 }
