1e754a14768772b7973ad36921ae088cc1294398
Aman Kumar Thakur <ssamanthakur@gmail.com>
2024-01-15 04:56:36 +0530

    some changes and started

--- 15QuickSort.cpp
+++ 15QuickSort.cpp
@@ -1,50 +1,56 @@
-// #include<bits/stdc++.h>
-// int P(vector<int>& v,int low,int high)
-// {
-//     int pivot=v[low];
-//     int i=low;
-//     int j=high;
-//     while(i<j)
-//     {
-//         while (v[i]<= pivot && i<=high-1)
-//         {
-//             i++;
-//         }
-//         while(v[j]>pivot && j>=low+1)
-//         {
-//             j--;
-//         }
-//         if(i<j)
-//         {
-//             swap(v[i],v[j]);
-//         }
-//     }
-//     swap(v[low],v[high]);
-//     return j;
-// }
-// void Qs(vector<int>& v,int low,int high)
-// {
-//     if(low<high)
-//     {
-//         int partition=P(v,low,high);
-//         Qs(v,low,partition-1);
-//         Qs(v,partition+1,high);
-//     }
-// }
-// int main()
-// {
-//     int n,input=0;
-//     cout<<"Enter No. of elements\n";
-//     cin>>n;
-//     vector<int>v(n);
-//     int low=0,high=v.size()-1;
-//     cout<<"Enter elements:\n";
-//     for(int i=0;i<n;i++)
-//     {
-//         cin>>input;
-//         v.push_back(input);
-//     }
-//     Qs(v,low,high);
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int partition(int arr[],int low,int high)
+{
   
-//     return 0;
-// }
+   
+  int pivot=arr[high];
+  
+  int i=(low-1);
+   
+  for(int j=low;j<=high;j++)
+  {
+    
+    if(arr[j]<pivot)
+    {
+      
+      i++;
+      swap(arr[i],arr[j]);
+    }
+  }
+  swap(arr[i+1],arr[high]);
+  return (i+1);
+}
+ 
+
+            
+void quickSort(int arr[],int low,int high)
+{
+  
+  if(low<high)
+  {
+    
+     
+    int pi=partition(arr,low,high);
+     
+    
+    quickSort(arr,low,pi-1);
+    quickSort(arr,pi+1,high);
+  }
+}
+              
+  
+int main() {
+  int arr[]={10,7,8,9,1,5};
+  int n=sizeof(arr)/sizeof(arr[0]);
+  
+  quickSort(arr,0,n-1);
+  
+  cout<<"Sorted Array\n";
+  for(int i=0;i<n;i++)
+  {
+    cout<<arr[i]<<" ";
+  }
+  return 0;
+}
---
+++ 16FindSecondLargestElementsInAnArray.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    vector<int>v(n);
+    
+    return 0;
+}
