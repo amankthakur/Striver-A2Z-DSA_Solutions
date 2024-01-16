f64826ce8a1850b18c1a380c7b9f6a390455272f
Aman Kumar Thakur <ssamanthakur@gmail.com>
2024-01-16 12:44:43 +0530

    completed 16 th vid and solved list questions

--- 15QuickSort.cpp
+++ 15QuickSort.cpp
@@ -1,15 +1,15 @@
 // #include <bits/stdc++.h>
 // using namespace std;
- 
+
 // int partition(int arr[],int low,int high)
 // {
 //   //choose the pivot
-   
+
 //   int pivot=arr[high];
 //   //Index of smaller element and Indicate
 //   //the right position of pivot found so far
 //   int i=(low-1);
-   
+
 //   for(int j=low;j<=high;j++)
 //   {
 //     //If current element is smaller than the pivot
@@ -23,18 +23,18 @@
 //   swap(arr[i+1],arr[high]);
 //   return (i+1);
 // }
- 
+
 // // The Quicksort function Implement
-            
+
 // void quickSort(int arr[],int low,int high)
 // {
 //   // when low is less than high
 //   if(low<high)
 //   {
 //     // pi is the partition return index of pivot
-     
+
 //     int pi=partition(arr,low,high);
-     
+
 //     //Recursion Call
 //     //smaller element than pivot goes left and
 //     //higher element goes right
@@ -42,8 +42,7 @@
 //     quickSort(arr,pi+1,high);
 //   }
 // }
-              
-  
+
 // int main() {
 //   int arr[]={10,7,8,9,1,5};
 //   int n=sizeof(arr)/sizeof(arr[0]);
@@ -56,4 +55,55 @@
 //     cout<<arr[i]<<" ";
 //   }
 //   return 0;
+// }
+
+
+
+// Another eg using vector\
+
+// #include <bits/stdc++.h>
+// int p(vector<int> &v, int low, int high)
+// {
+//     int pivot = v[high];
+//     int i = (low - 1);
+//     for (int j = low; j <= high; j++)
+//     {
+//         if (v[j] < pivot)
+//         {
+//             i++;
+//             swap(v[i], v[j]);
+//         }
+//     }
+//     swap(v[i + 1], v[high]);
+//     return (i + 1);
+// }
+// void qs(vector<int> &v, int low, int high)
+// {
+//     if (low < high)
+//     {
+//         int pi = p(v, low, high);
+//         qs(v, low, pi - 1);
+//         qs(v, pi + 1, high);
+//     }
+// }
+// int main()
+// {
+//     int n;
+//     cout << "Enter no. of elements in unsorted array" << endl;
+//     cin >> n;
+//     vector<int> v;
+//     cout << "Enter elements in unsorted array" << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         int input;
+//         cin >> input;
+//         v.push_back(input);
+//     }
+//     qs(v, 0, n - 1);
+//     cout << "Sorted array is: " << endl;
+//     for (auto it : v)
+//     {
+//         cout << it << " ";
+//     }
+//     return 0;
 // }
--- 16FindSecondLargestElementsInAnArray.cpp
+++ 16FindSecondLargestElementsInAnArray.cpp
@@ -1,9 +1,126 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+// int largest(int* arr,int n)
+// {
+//     int largest=arr[0];
+//     for(int i=0;i<n;i++)
+//     {
+//         if(arr[i]>largest)
+//         {
+//             largest=arr[i];
+//         }
+//     }
+//     return largest;
+// }
+
+// void seclarsecsmall(int* arr,int n)
+// {
+//     int count=0;
+//     map<int,int>mpp;
+//     for(int i=0;i<n;i++)
+//     {
+//         mpp[arr[i]]++;
+//     }
+//     for(auto it : mpp)
+//     {
+
+//         if(count==(mpp.size()-(mpp.size()-1)))
+//         {
+//             cout<<it.first<<" ";
+//         }
+
+//         if(count==(mpp.size()-2))
+//         {
+//         cout<<it.first;
+//         break;
+//         }
+//         count++;
+//     }
+// }
+
+// bool ifsorted(int* arr,int n)
+// {
+//     int small = arr[0];
+//     for(int i=1;i<n;i++)
+//     {
+//         if(arr[i]<small)
+//         {
+//             return false;
+//         }
+//         small=arr[i];
+//     }
+//     return true;
+// }
+
+// int removeDuplicates(int* arr, int n) {
+//     int pos;
+//     for(int i=1;i<n;i++)
+//    {
+//     if(arr[i-1]==arr[i])
+//     {
+//         for(int j=i;j<n-1;j++)
+//         {
+//             arr[j]=arr[(j+1)%n];
+//             i--;
+//         }
+//         arr[n-1]=NULL;
+//     }
+//    }
+//    while(n>0)
+//    {
+//     if(arr[n]==0)
+//     {
+//         n--;
+//     }
+//     else
+//     {
+//         pos=n;
+//         break;
+//     }
+//    }
+//    return (pos+1);
+// }
+
+// int removeDuplicates(vector<int> &arr, int n) {
+//     int i=0;
+//     for(int j=1;j<n;j++)
+//     {
+//         if(arr[j]!=arr[i])
+//         {
+//             i++;
+//             arr[i]=arr[j];
+//         }
+//     }
+//     return (i+1);
+// }
 int main()
 {
     int n;
-    vector<int>v(n);
-    
+    cout << "Enter No. of Elements in array:\n";
+    cin >> n;
+    int arr[n];
+    cout << "Enter Elements in array:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Largest element in an array
+    // cout<<"Largest Element:\n"<<largest(arr,n);
+
+    // 2nd largest and 2nd smallest element in an array
+    // seclarsecsmall(arr,n);
+
+    // Check if array is Sorted
+    // cout<<ifsorted(arr,n);
+
+    // To remove Duplicate elements and tell the new size of array.
+    // Brute force approch
+    // cout<<removeDuplicates(arr,n);
+
+    // To remove Duplicate elements and tell the new size of array.
+    // Same question with optimal approch
+    // cout<<removeDuplicates(arr,n);
+
     return 0;
 }
